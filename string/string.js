c9a0339ce8887a550fce42a4b668b64c50d9eec9
Christopher Stone <cmstone@student.fullsail.edu>
2022-03-06 20:20:33 -0500

    New test for the string module - all passed

---
+++ string/string.js
@@ -0,0 +1,9 @@
+const uppercase = (str) => str.toUpperCase()
+
+// lowercase function to lowercase text
+const lowercase = (str) => str.toLowerCase()
+
+module.exports = {
+    uppercase,
+    lowercase
+}
---
+++ string/string.test.js
@@ -0,0 +1,15 @@
+const {
+    uppercase,
+    lowercase
+} = require("./string")
+
+describe("Testing the string module", () => {
+
+    test("should uppercase a string input", () => {
+        expect(uppercase("bob")).toBe("BOB")
+    })
+
+    test("should lowercase a string input", () => {
+        expect(lowercase("FULL SAIL")).toBe("full sail")
+    })
+})
